7767bbc392096b9fcc416e4557465b082ac6d99c
neonithinar <neonithinar@gmail.com>
2021-06-23 00:08:59 +0530

    Created My blog, added files and codes

---
+++ blog/views.py
@@ -0,0 +1,70 @@
+from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+	ListView, 
+	DetailView, 
+	CreateView, 
+	UpdateView, 
+	DeleteView
+)
+from .models import Post
+
+
+
+def home(request):
+	context = {
+		'posts':Post.objects.all()
+	}
+	return render(request, 'blog/home.html', context)
+
+class PostListView(ListView):
+	model = Post
+	template_name = 'blog/home.html' # by default <app>/<model>_<viewtype>.html
+	context_object_name = 'posts' # default object_list
+	ordering = ['-date_posted'] # remove '-' sign so that latest post appear last
+	paginate_by = 5
+
+
+class PostDetailView(DetailView):
+	model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+	model = Post
+	fields = ['title', 'content']
+
+	def form_valid(self, form):
+		form.instance.author = self.request.user
+		return super().form_valid(form)	
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+	model = Post
+	fields = ['title', 'content']
+
+	def form_valid(self, form):
+		form.instance.author = self.request.user
+		return super().form_valid(form)	
+
+
+	def test_func(self):
+		post = self.get_object()
+		if self.request.user == post.author:
+			return True
+		return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+	model = Post
+	success_url = '/'
+
+	def test_func(self):
+		post = self.get_object()
+		if self.request.user == post.author:
+			return True
+		return False	
+
+
+def about(request):
+	return render(request, 'blog/about.html', {'title': 'About Page AMP'})
+

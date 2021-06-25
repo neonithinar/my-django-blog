4648252e60522e9d4c619b03f0b181163dd7a281
neonithinar <neonithinar@gmail.com>
2021-06-25 19:52:10 +0530

    reverted the author link to author based posts

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5^ddg^8kgxq19w%2vi487pz%ej+lr0gucsnra7!(%!i%ogn%2a'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

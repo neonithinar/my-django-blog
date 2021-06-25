e7f7276e3393a2a10cf4a3801a8fd2e408a7db65
neonithinar <neonithinar@gmail.com>
2021-06-25 18:51:40 +0530

    Created password-reset route, added a bunch of std files to .gitignore

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 import os
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -125,5 +127,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('DB_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('DB_PASS')
+EMAIL_HOST_USER = config('MAIL_USER')
+EMAIL_HOST_PASSWORD = config('MAIL_PASS')
